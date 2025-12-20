d23e0d0c7b8ff16f6331184d5892d9edb65d18a6
mumjk <getbackmyacccount@gmail.com>
2025-12-20 23:26:59 +0700

    chore: setup project core, Hilt DI and Firebase configuration

---
+++ app/src/main/java/com/example/se114/di/AppModule.kt
@@ -0,0 +1,38 @@
+package com.example.se114.di
+
+import android.content.Context
+import com.example.se114.local.PreferencesManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    // Cung cấp Firebase Auth
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    // Cung cấp Firebase Firestore
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
+    // Cung cấp PreferencesManager để Inject vào ViewModel
+    @Provides
+    @Singleton
+    fun providePreferencesManager(@ApplicationContext context: Context): PreferencesManager {
+        return PreferencesManager(context)
+    }
+}
